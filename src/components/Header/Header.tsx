3c741108b2d4303d5c3e5df0f34e6b5a436b9b83
Eloy Bermejo <eloybercast@gmail.com>
2024-11-24 21:29:05 +0100

    Creating first version of the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
+import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <header className={styles.header}>
-      <section className={`debug ` + styles.logoContainer}></section>
-      <section className={`debug ` + styles.navContainer}></section>
-      <section className={`debug ` + styles.sessionContainer}></section>
+      <section className={styles.logoContainer}></section>
+      <section className={styles.navContainer}>
+        <GiHamburgerMenu size={30} className={styles.hamburgerIcon} />
+      </section>
+      <section className={styles.sessionContainer}>
+        <LoginButton></LoginButton>
+      </section>
     </header>
   );
+}
+
+function LoginButton() {
+  return <button className="accent">Sign in</button>;
 }
 
 export default Header;
