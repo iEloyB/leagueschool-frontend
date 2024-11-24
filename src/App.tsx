459711af162637caaa2ee29ed31d59d22f163be3
Eloy Bermejo <eloybercast@gmail.com>
2024-11-24 19:34:08 +0100

    Adding main css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NotFound from "./pages/NotFound/NotFound";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<NotFound />} />
