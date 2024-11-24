d7dd11becf477b8367c239b1f3ed0e93422ab1c0
Eloy Bermejo <eloybercast@gmail.com>
2024-11-24 20:08:30 +0100

    Creating header schema

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
 import styles from "./Header.module.css";
 
 function Header() {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <section className={`debug ` + styles.logoContainer}></section>
+      <section className={`debug ` + styles.navContainer}></section>
+      <section className={`debug ` + styles.sessionContainer}></section>
+    </header>
+  );
 }
 
 export default Header;
