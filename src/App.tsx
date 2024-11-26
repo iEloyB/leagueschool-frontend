71df05b90411e3ddb13c71851abd7bf136fab360
Eloy Bermejo <eloybercast@gmail.com>
2024-11-26 21:18:40 +0100

    Adding analytics into the react app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NotFound from "./pages/NotFound/NotFound";
 import Header from "./components/Header/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
